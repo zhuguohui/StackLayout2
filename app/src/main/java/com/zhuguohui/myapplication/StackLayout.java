a86f5a0c14a9145b3b0ae84132dd23b13f8fc0a5
zhuguohui <287718603@qq.com>
2023-04-20 22:24:56 +0800

    Initial commit

---
+++ app/src/main/java/com/zhuguohui/myapplication/StackLayout.java
@@ -0,0 +1,22 @@
+package com.zhuguohui.myapplication;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.FrameLayout;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+/**
+ * Created by zhuguohui
+ * Date: 2023/4/20
+ * Time: 21:37
+ * Desc:
+ */
+public class StackLayout extends FrameLayout {
+
+
+    public StackLayout(@NonNull Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+}
