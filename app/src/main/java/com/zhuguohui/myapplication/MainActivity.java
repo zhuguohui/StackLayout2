ac615bde965f4179e86edd262f94644df392a12e
zhu.guohui <zhu.guohui@trs.com.cn>
2023-05-18 15:07:41 +0800

    完成方向的约束

--- app/src/main/java/com/zhuguohui/myapplication/MainActivity.java
+++ app/src/main/java/com/zhuguohui/myapplication/MainActivity.java
@@ -3,6 +3,7 @@ package com.zhuguohui.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        findViewById(R.id.btn_like).setOnClickListener(v->{
+            Toast.makeText(this,"点赞成功",Toast.LENGTH_SHORT).show();
+        });
     }
 }
